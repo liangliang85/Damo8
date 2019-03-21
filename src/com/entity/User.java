51fcbb321a6a8c97561b15c6c1f45d168c94a774
lll <liang63065618@126.com>
2019-03-21 12:11:46 +0800

    ll  提交

---
+++ src/com/entity/User.java
@@ -0,0 +1,10 @@
+package com.entity;
+
+/**
+ * Created by Administrator on 2019/3/21.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("ssssssssssss");
+    }
+}
